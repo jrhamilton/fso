6f8735134e00faa2126ade716218200fad98abfc
Jason H <jason@localhostMail.com>
2022-12-28 11:13:55 -0500

    exercises 1.9 and 1.10 completed.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = (props) => {
   return (
-    <div>
-      <h1>statistics</h1>
-      <div>
-        <p>good {props.good}</p>
-        <p>nuetral {props.nuetral}</p>
-        <p>bad {props.bad}</p>
-        <p>all {props.all}</p>
-        <p>average {props.average}</p>
-      </div>
-    </div>
+    <p>{props.text} {props.value}</p>
   )
+}
+
+const Statistics = (props) => {
+  if (props.activate) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <div>
+          <StatisticLine text='good' value={props.good} />
+          <StatisticLine text='nuetral' value={props.nuetral} />
+          <StatisticLine text='bad' value={props.bad} />
+          <StatisticLine text='all' value={props.all} />
+          <StatisticLine text='average' value={props.average} />
+        </div>
+      </div>
+    )
+  }
 }
 
 const App = () => {
@@ -22,34 +36,40 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
   const [average, setAverage] = useState(0)
+  const [activate, setActivate] = useState(false)
+
+  const handled = () => {
+    setAll(all + 1)
+    setActivate(true)
+  }
 
   const theGood = () => {
-    setAll(all + 1)
-    setGood(good + 1)
     setAverage((good + 1 - bad) / (all + 1))
+    setGood(good + 1)
+    handled()
   }
 
   const theNuetral = () => {
-    setAll(all + 1)
+    setAverage((good - bad) / (all + 1))
     setNuetral(nuetral + 1)
-    setAverage(average)
+    handled()
   }
 
   const theBad = () => {
-    setAll(all + 1)
-    setBad(bad + 1)
     setAverage((good - bad - 1) / (all + 1))
+    setBad(bad + 1)
+    handled()
   }
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={theGood}>good</button>
-        <button onClick={theNuetral}>nuetral</button>
-        <button onClick={theBad}>bad</button>
+        <Button handleClick={() => theGood()} text='good' />
+        <Button handleClick={() => theNuetral()} text='nuetral' />
+        <Button handleClick={() => theBad()} text='bad' />
       </div>
-      <Statistics good={good} nuetral={nuetral} bad={bad} all={all} average={average} />
+      <Statistics good={good} nuetral={nuetral} bad={bad} all={all} average={average} activate={activate} />
     </div>
   )
 }
