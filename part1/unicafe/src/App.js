cb4f3e6bf4eb45285bc3feb50d563b60dccef93d
Jason H <jason@localhostMail.com>
2022-12-28 09:30:01 -0500

    Exercise 1.7 completed. Functions not DRY though

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,20 +5,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [nuetral, setNuetral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+
+  const theGood = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+    setAverage((good + 1 - bad) / (all + 1))
+  }
+
+  const theNuetral = () => {
+    setAll(all + 1)
+    setNuetral(nuetral + 1)
+    setAverage(average)
+  }
+
+  const theBad = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+    setAverage((good - bad - 1) / (all + 1))
+  }
+
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNuetral(nuetral + 1)}>nuetral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={theGood}>good</button>
+        <button onClick={theNuetral}>nuetral</button>
+        <button onClick={theBad}>bad</button>
       </div>
       <h1>statistics</h1>
       <div>
         <p>good {good}</p>
         <p>nuetral {nuetral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
       </div>
     </div>
   )
