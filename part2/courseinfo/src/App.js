8a0a23b20b06d34245cb4c0221accc396e2a8f79
Jason H <jason@localhostMail.com>
2022-12-29 12:23:02 -0500

    Exercise 2.1 completed.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,48 +2,59 @@ const Header = ({ course }) => <h1>{course}</h1>
 
 const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
+const Part = (props) => {
+  return (
+  <li>
+    {props.name} {props.exercises}
+  </li>
+)
+}
 
-const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+const Parts = ( {parts} ) => (
+  <ul style={{listStyleType:"none", paddingInlineStart:"0"}}>
+    {parts.map(part =>
+      <Part key={part.id.toString()} name={part.name} exercises={part.exercises} />
+    )}
+  </ul>
+)
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+const Course = ({course}) => {
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <ul style={{listStyleType:"none"}}>
+        <li key={course.id}>
+          <Header course={course.name} />
+          <Parts parts={course.parts} />
+        </li>
+      </ul>
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
