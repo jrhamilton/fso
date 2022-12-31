f9662feb348cff0761af6c7d3046d8caf652798f
Jason H <jason@localhostMail.com>
2022-12-31 04:45:20 -0500

    Exercise 2.6 completed. Name submission and list.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,12 +6,43 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const Person = (props) => (
+    <li>
+      {props.name}
+    </li>
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
+        <ul>
+          {persons.map(person =>
+            <Person key={person.name}
+                    name={person.name} />
+          )}
+        </ul>
         <div>
-          name: <input />
+          name:
+          <input value={newName}
+                 onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
