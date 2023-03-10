497b05404c917c114584f10d999a62d2432ed6c5
Jason H <jason@localhostMail.com>
2023-03-10 10:17:02 -0500

    3.10 & 3.11. Fly deployment, README and build.

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -106,7 +106,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Phonebook server running on port ${PORT}`)
 })
